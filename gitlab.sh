83e56ee6449eacc7b2940276e50ba5b43c5ae32d
zaima? <1206691662@outlook.com>
2023-02-19 11:28:01 +0800

    update

--- gitlab.sh
+++ gitlab.sh
@@ -17,3 +17,15 @@ sudo systemctl enable postfix
 sudo systemctl start postfix
 
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
+
+sudo yum install -y gitlab-ee
+
+sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+
+sudo gitlab-ctl stop
+
+sudo gitlab-ctl reconfigure
+
+sudo gitlab-ctl start
+
+cat /etc/gitlab/initial_root_password
